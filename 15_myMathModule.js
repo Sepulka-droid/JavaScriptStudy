cc3016c2956f14c4edf479c3035bcf7f375767af
Sepulka-droid <deutherium@yandex.ru>
2022-07-30 09:44:44 +0300

    deleting old files

--- 15_myMathModule.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-const getTrianglePerimeter = (a, b, c) => a + b + c;
-
-// BEGIN (write your solution here)
-const getTriangleArea = (h, b) => (h * b) / 2;
-
-export { getTrianglePerimeter, getTriangleArea };
-// END
--- test.js
+++
@@ -1,2 +0,0 @@
-let who = "dragon's" + 'mother';
-console.log(who);
